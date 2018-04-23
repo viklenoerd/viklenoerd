9afca472ea83561a946caa699dbe59c7514f735d
David Wolff <info@davidwolff.dk>
2018-04-23 14:12:14 +0200

    boilerplate - connect to contelful and url rel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Link from '../util/Link'
 
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
+
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Link to="/about">About</Link>
+    <br />
+    <Link to="http://www.google.com">Google</Link>
   </div>
 )
 
